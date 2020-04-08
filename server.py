971ae411d544b3706866afd11896b33c1f6f5ad3
FireFeathers06 <harshit14feb@gmail.com>
2020-04-08 18:15:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -11,6 +11,11 @@ thresolds = {
     "LOW": [2,10],
     "VERYLOW": [0,2]
 }
+def date_format(date):
+    date = date.split('/')
+    date = date[1] + '/' + date[0]
+    return date
+
 @route('/res/<filepath:path>', name='res')
 def resources(filepath):
     return static_file(filepath, root='./res')
@@ -22,42 +27,55 @@ def homepage():
 @route('/country/<argu>')
 def countries(argu):
     url_to_get = f"https://corona.lmao.ninja/v2/historical/{argu}?lastdays=100"
+    total_cases_url = f"https://corona.lmao.ninja/countries/{argu}"
     datatosend = {}
+    total_cases = requests.get(total_cases_url).json()['cases']
     data = requests.get(url_to_get).json()
-    x_axis = []
-    y_axis = []
+    dates = []
     try:
-        data = {key:value for key, value in data['timeline']['cases'].items() if value != 0}
         new_dict = {}
-        for key, value in data.items():
+        new_data = {key:value for key, value in data['timeline']['cases'].items() if value != 0}
+        # print(new_data)
+        for key, value in new_data.items():
             new_dict_count = list(new_dict.values()).count(value)
-            old_dict_count = list(data.values()).count(value)
-            if new_dict_count < old_dict_count / 2:
+            old_dict_count = list(new_data.values()).count(value)
+            if new_dict_count < (old_dict_count / 2):
                 new_dict[key] = value
-        for date, cases in new_dict.items():
-            date = date.split('/')
-            date = date[1] + '/' + date[0]
-            x_axis.append(date)
-            y_axis.append(cases)
+                dates.append(date_format(key))
+                # print(date_format(key))
+        # print(new_dict)
+        datatosend['dates'] = [0] + dates
+        
+        for i in data['timeline']:
+            y_axis = []
+            for key, value in data['timeline'][i].items():
+                if date_format(key) in dates:
+                    y_axis.append(value)
+            datatosend[i] = [0] + y_axis
+            
     except KeyError:
         return data['message']
-    datatosend['x'] = [0] + x_axis
-    datatosend['y'] = [0] + y_axis
-    datatosend['length'] = len(x_axis)
+    datatosend['length'] = len(dates) + 1
     datatosend['country'] = argu.title()
+    datatosend['total_cases'] = format(total_cases, ',d')
+    print(datatosend)
     return template('countryGraph.tpl', url = url, datalist=datatosend)
 
 @route("/worldmap/<argu>" , name="worldmap")
 def world_map(argu):
     datatosend = {}
-    appdata = json.load(open("app.json" ,"r"))
+    with open("app.json" ,"r") as f:
+        appdata = json.load(f)
     if datetime.datetime.now().timestamp()-100000 > appdata["time"] :
         appdata["time"] = datetime.datetime.now().timestamp()
-        json.dump(appdata,open("app.json","w"))
+        with open("app.json" ,"w") as f:
+            json.dump(appdata, f)
         datalist = requests.get(appdata["url"]).json()
-        json.dump(datalist ,open("datalist.json" , "w"))
+        with open("datalist.json" ,"w") as f:
+            json.dump(datalist, f)
     else:
-        datalist = json.load(open("datalist.json" , 'r'))
+        with open("datalist.json" , 'r') as f:
+            datalist = json.load(f)
     if argu not in datalist[0].keys():
         return "Wrong Argument"
     tempf = []
@@ -91,14 +109,14 @@ def world_map(argu):
             except:
                 continue
     tmpl = '''
-        <span class="popup">total ${window.location.pathname.split('/')[2]}(${geo.properties.name}) : ${data.numberOfThings}</span>
+        <span class="popup">Total ${window.location.pathname.split('/')[2]}(${geo.properties.name}) : ${data.numberOfThings}</span>
 
     '''
     return template('worldmap.tpl' ,url=url ,datalist=datatosend)
 
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5000))
-     run(host="0.0.0.0" , port=port , debug= False)
+    port = int(os.environ.get("PORT", 5000))
+    run(host="0.0.0.0" , port=port , debug= False)
     # run(debug=True)
 
